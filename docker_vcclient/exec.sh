e6ad2ccff2e3b1eadacbe4ae1e597ee9b14bf69b
Tomoki Takasawa <33464999+ttakasawa@users.noreply.github.com>
2023-09-05 23:15:10 -0700

    Update exec.sh

--- docker_vcclient/exec.sh
+++ docker_vcclient/exec.sh
@@ -5,12 +5,26 @@ set -eu
 # cp -r /weights/* /voice-changer/server/
 # /bin/bash
 
-python3 MMVCServerSIO.py $@
+# python3 MMVCServerSIO.py $@
 # python3 MMVCServerSIO.py -p 18888 --https true \
 #   --content_vec_500 checkpoint_best_legacy_500.pt \
 #   --hubert_base hubert_base.pt \
 #   --hubert_soft hubert-soft-0d54a1f4.pt \
 #   --nsf_hifigan nsf_hifigan/model
+
+python3 MMVCServerSIO.py -p 18888 --https true \
+    --content_vec_500 pretrain/checkpoint_best_legacy_500.pt  \
+    --content_vec_500_onnx pretrain/content_vec_500.onnx \
+    --content_vec_500_onnx_on true \
+    --hubert_base pretrain/hubert_base.pt \
+    --hubert_base_jp pretrain/rinna_hubert_base_jp.pt \
+    --hubert_soft pretrain/hubert/hubert-soft-0d54a1f4.pt \
+    --nsf_hifigan pretrain/nsf_hifigan/model \
+    --crepe_onnx_full pretrain/crepe_onnx_full.onnx \
+    --crepe_onnx_tiny pretrain/crepe_onnx_tiny.onnx \
+    --rmvpe pretrain/rmvpe.pt \
+    --model_dir model_dir \
+    --samples samples.json
 
 
 # -p 18888 --https true \

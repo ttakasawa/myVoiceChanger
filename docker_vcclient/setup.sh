e92471a14063d46466c1c6685097e4d47f8fa083
Tomoki Takasawa <33464999+ttakasawa@users.noreply.github.com>
2023-09-05 22:46:58 -0700

    Update setup.sh

--- docker_vcclient/setup.sh
+++ docker_vcclient/setup.sh
@@ -10,7 +10,7 @@ GROUP_ID=${LOCAL_GID:-9002}
 echo "exec with [UID : $USER_ID, GID: $GROUP_ID]"
 # useradd -u $USER_ID -o -m user
 # groupmod -g $GROUP_ID user
-chmod uog+rwx tmp_dir
+# chmod uog+rwx tmp_dir
 
 #su user
 #echo "parameter: $@"
